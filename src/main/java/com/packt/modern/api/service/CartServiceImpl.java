f5edb078337d264ba4163f6b0a1e91379131aff8
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-28 22:31:03 +0530

    Implemented item service

--- src/main/java/com/packt/modern/api/service/CartServiceImpl.java
+++ src/main/java/com/packt/modern/api/service/CartServiceImpl.java
@@ -3,12 +3,28 @@ package com.packt.modern.api.service;
 import com.packt.modern.api.entity.CartEntity;
 import com.packt.modern.api.entity.ItemEntity;
 import com.packt.modern.api.model.Item;
+import com.packt.modern.api.repository.CartRepository;
+import com.packt.modern.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CartServiceImpl implements CartService {
+
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
+    private final ItemService itemService;
+
+    @Autowired
+    public CartServiceImpl(final CartRepository cartRepository,
+                           final UserRepository userRepository,
+                           final ItemService itemService) {
+        this.cartRepository = cartRepository;
+        this.userRepository = userRepository;
+        this.itemService = itemService;
+    }
     @Override
     public List<Item> addItemsByCustomerId(String customerId, Item item) {
         return null;
--- src/main/java/com/packt/modern/api/service/ItemServiceImpl.java
+++ src/main/java/com/packt/modern/api/service/ItemServiceImpl.java
@@ -1,30 +1,41 @@
 package com.packt.modern.api.service;
 
 import com.packt.modern.api.entity.ItemEntity;
+import com.packt.modern.api.entity.ProductEntity;
 import com.packt.modern.api.model.Item;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemServiceImpl implements ItemService {
     @Override
     public ItemEntity toEntity(Item item) {
-        return null;
+        return new ItemEntity().setProduct(
+            new ProductEntity().setId(UUID.fromString(item.getId())))
+        .setQuantity(item.getQuantity())
+        .setPrice(item.getUnitPrice());
     }
 
     @Override
     public List<ItemEntity> toEntityList(List<Item> itemList) {
-        return null;
+        if (Objects.isNull(itemList)) {
+            return List.of();
+        }
+        return itemList.stream().map(this::toEntity).collect(Collectors.toList());
     }
 
     @Override
     public Item toModel(ItemEntity itemEntity) {
-        return null;
+        return new Item().quantity(itemEntity.getQuantity()).unitPrice(
+                itemEntity.getPrice()).id(itemEntity.getProduct().getId().toString());
     }
 
     @Override
     public List<Item> toModelList(List<ItemEntity> itemEntities) {
-        return null;
+        return itemEntities.stream().map(this::toModel).collect(Collectors.toList());
     }
 }
